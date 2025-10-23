088dc979ab2f22abd925055dda7b6bbc9c12ca95
Jenny HY Lin <jennyhylin@gmail.com>
2025-10-23 14:28:05 +1300

    Updated .qmd using reticulate & added small .py script

--- read_df.py
+++ read_df.py
@@ -4,6 +4,6 @@ import pandas as pd
 def reading_df(file):
     # Reading a df (using one of my old cleaned datasets from a while ago)
     df = pd.read_csv(file)
-    df = df.rename(columns={"QED Weighted": "QED"})
+    df = df.rename(columns={"QED Weighted": "QED", "Polar Surface Area": "PSA"})
     
-    return df.head()
+    return df
