d6de0f46cbff81210d6c9d246a2e24f47a394967
Jenny HY Lin <jennyhylin@gmail.com>
2026-08-08 14:57:43 +1200

    Testing py2opsin

--- smiles_fg.py
+++ smiles_fg.py
@@ -17,7 +17,7 @@ app = marimo.App(width="full")
 def _():
     import marimo as mo
 
-    return
+    return (mo,)
 
 
 @app.cell
@@ -30,7 +30,8 @@ def _():
 
 @app.cell
 def _(Chem):
-    mol = Chem.MolFromSmiles('COC(=O)CNC(=O)CCCO')
+    #mol = Chem.MolFromSmiles('COC(=O)CNC(=O)CCCO')
+    mol = Chem.MolFromSmiles("C1=CC=CC=C1")
     mol
     return (mol,)
 
@@ -45,6 +46,8 @@ def _(Chem, mol):
 
 @app.cell
 def _():
+    # https://github.com/dan2097/opsin
+    # https://github.com/JacksonBurns/py2opsin
     from py2opsin import py2opsin
 
     return (py2opsin,)
@@ -53,11 +56,24 @@ def _():
 @app.cell
 def _(py2opsin):
     smiles_string = py2opsin(
-        chemical_name = "ethane",
+        chemical_name = ["ethane", "cyclopentane", "benzene", "pyridine"], 
         output_format = "SMILES",
     )
 
     smiles_string
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    There are different types of SMILES available...
+    """)
+    return
+
+
+@app.cell
+def _():
     return
 
 
