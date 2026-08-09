7ee951a20e80c1bbaa3db6a9984434dc16e9bcc6
Jenny HY Lin <jennyhylin@gmail.com>
2026-08-09 12:31:06 +1200

    Added info. re. starting marimo nb

--- smiles_fg.py
+++ smiles_fg.py
@@ -13,8 +13,23 @@ __generated_with = "0.23.16"
 app = marimo.App(width="full")
 
 
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    **Starting a Marimo notebook**
+
+    To create or edit a marimo notebook, use the command: *marimo edit notebook_filename.py* in the terminal ([reference link](https://docs.marimo.io/getting_started/quickstart/) to show other related commands). It will then ask if you'd like to run this notebook in a sandboxed venv containing this notebooks's dependencies. By answering yes, this'll immediately take you to the notebook opened in your default web browser. This requires marimo to be installed earlier, and this Marimo [reference link](https://docs.marimo.io/getting_started/installation/) shows how to do it. Marimo notebooks work well with uv, a Python package installer and manager.
+
+    The only other thing to note here is that Marimo notebooks currently only work with Python programming language and not other languages (unless there are new plans announced from the Marimo team).
+
+    Marimo notebook apparently exists as a Python script so if it is opened in an editor (e.g. VS Code), it'll look different from how it appears in a browser as the code inside the notebook will look like it's for an app.
+    """)
+    return
+
+
 @app.cell
 def _():
+    # apparently need this line below so markdown cells can be used
     import marimo as mo
 
     return (mo,)
